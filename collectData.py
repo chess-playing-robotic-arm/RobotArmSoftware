225a44cd3d969abcc9e9f8dc9bdd0f4852b8edb0
Youssef Darahem <youssef.darahem@gmail.com>
2023-03-10 10:40:45 +0300

    cleaned

--- collectData.py
+++ collectData.py
@@ -1,4 +1,4 @@
-from boardDetectFun import crop, drawOrderedPoints, readPoints, writeOutSquares
+from boardDetectFun import crop, readPoints, writeOutSquares
 import numpy as np
 from square import Square
 import cv2 as cv
@@ -7,23 +7,21 @@ import cv2 as cv
 
 def main():
   
-    cap = cv.VideoCapture(1)
+    cap = cv.VideoCapture(0)
     _, img = cap.read()
    
     setup = True
-    boardimg = []
-    boardrow = []
+    board_img = []
+    board_row = []
 
-    boardimg = []
-    boardrow = []
-    index = 0
+
 
     points = readPoints()
 
     for i in range(9):
         points[i] = sorted(points[i], reverse=True)
 
-    squareid = 64
+    square_id = 64
 
     for i in range(8):
         for j in range(8):
@@ -31,56 +29,48 @@ def main():
             p2 = points[i][j+1]
             p3 = points[i+1][j]
             p4 = points[i+1][j+1]
-            square = Square(p1=p1, p2=p2, p3=p3, p4=p4, position=squareid)
-            squareid -= 1
-            boardrow.append(square)
-        boardimg.append(boardrow)
-        boardrow = []
+            square = Square(p1=p1, p2=p2, p3=p3, p4=p4, position=square_id)
+            square_id -= 1
+            board_row.append(square)
+        board_img.append(board_row)
+        board_row = []
 
-    boardimg = np.transpose(boardimg)
-    boardimg = np.flip(boardimg, 1)
-    boardimg = np.flip(boardimg, 0)
+    board_img = np.transpose(board_img)
+    board_img = np.flip(board_img, 1)
+    board_img = np.flip(board_img, 0)
 
-    squareid = 1
+    square_id = 1
     for i in range(8):
         for j in range(8):
-            boardimg[i][j].position = squareid
-            squareid += 1
+            board_img[i][j].position = square_id
+            square_id += 1
 
-    index = 320
+    index = 23168
     while setup:
         print('setup started !!')
         sqRow = []
         sqs = []
         for i in range(8):
             for j in range(8):
-                sq = crop(img, boardimg[i][j])
+                sq = crop(img, board_img[i][j])
                 sqRow.append(sq)
             sqs.append(sqRow)
             sqRow = []
 
-        # cv.imshow('image',img)
-        # keyCode = cv.waitKey(50) & 0xFF
-        print('make your choice : ')
-        userInput  = input()
-        if(userInput == 'q'):
-            print('quitting')
-            cap.release()
+        print('s is pressed ')
+        writeOutSquares(sqs,'./raw/',index=index)
+        print('continue ? (y/n)')
+        choice = input()
+        if choice == 'n':
+            setup = False
+
             break
-        elif(userInput == 's'):
-            print('s is pressed ')
-            writeOutSquares(sqs,'./raw/',index=index)
-            print('continue ? (y/n)')
-            choice = input()
-            if choice == 'y':
-                _, img = cap.read()
-                index += 64
-                setup = True
 
-            else:
-                setup = False
 
-                break
+        else:
+            _, img = cap.read()
+            index += 64
+            setup = True
 
     cv.waitKey(0)
     cv.destroyAllWindows()
