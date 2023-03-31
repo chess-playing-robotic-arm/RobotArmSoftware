02bdfdb1dec0af70ea023fdcdeddae29589f368b
Youssef Darahem <youssef.darahem@gmail.com>
2023-03-31 12:58:09 +0300

    add merge function

--- control_arm/arm_model/arm.py
+++ control_arm/arm_model/arm.py
@@ -2,11 +2,19 @@ import time
 
 RIGHT = 0
 LEFT = 1
-BASE = 2
+BASE= 2
 GRIPPER = 3
 STEP = 2
 
-moves = {'o':[16,48,145],'e2': [65,74,111],'e4':[61,72,97]}
+moves = {'o':[16,48,145],'e2': [64,72,114],'e4':[61,72,97]}
+
+
+def merge_alternatively(lst1, lst2):
+        if not lst1:
+            return lst2
+        if not lst2:
+            return lst1
+        return [lst1[0], lst2[0]] + merge_alternatively(lst1[1:], lst2[1:])
 
 
 class Motor():
@@ -21,12 +29,14 @@ class Motor():
     # returns: the list of commands that should be executed by the arm.
     def rotate(self,new_pos):
         commands = []
+        print(self.pos)
+        print(new_pos)
         if(self.pos < new_pos): # ex: 90 -> 180
-            for i in range(self.pos, new_pos ,STEP):
+            for i in range(self.pos, new_pos+1 ,STEP):
                 command = f'{self.id}{i}|'.encode()
                 commands.append(command)
-        elif(self.pos > new_pos):
-            for i in range(self.pos,new_pos,-STEP):
+        elif(self.pos > new_pos): # ex: 180 -> 90
+            for i in range(self.pos,new_pos-1,-STEP):
                 command = f'{self.id}{i}|'.encode()
                 commands.append(command)
         else:
@@ -34,15 +44,8 @@ class Motor():
         return commands
     
 
-
     def __str__(self):
-        return f'{self.id}: {self.name}'
-
-    
-
-    
-
-    
+        return self.name
 
 
 class Arm():
@@ -54,63 +57,50 @@ class Arm():
         self.gripper_is_open = gripper_is_open
 
     def open_gripper(self):
-        self.execute(GRIPPER,125)
+        self.arduino_conn.write('3125|'.encode())
         self.gripper_is_open = True
 
     def grab(self):
-        self.execute(GRIPPER,175)
+        self.arduino_conn.write('3175|'.encode())
         self.gripper_is_open = False
 
-
-
-    #TODO: there has to be a better way to refactor this junk
     def execute(self,motor,degree):
-        print(f'Motor is  : {motor}, degree to go to : {degree}')
-        
-        if (self.left is None or self.right is None):
-            command = f'{motor}{degree}|'.encode()
-            self.arduino_conn.write(command)
-            time.sleep(0.10) 
+        print(f'Motor is: {motor}, degree to go to : {degree}')
+        if(motor.pos == None):
+            print(f'{motor.pos}')
+            command = f'{motor.id}{degree}|'.encode()
+            print(command)
+            self.arduino_conn.write(command)   
         else:
-            if(motor == RIGHT):
-                if(degree < self.right): # trying to retract the right arm ex: 180 -> 40
-                    
-                    for i in range(self.right, degree ,-STEP):
-                        command = f'{motor}{i}|'.encode()
-                        self.arduino_conn.write(command)
-                        time.sleep(0.10) 
-                else: # ex: 40 -> 180
-                    for i in range(self.right, degree ,STEP):
-                        command = f'{motor}{i}|'.encode()
-                        self.arduino_conn.write(command)
-                        time.sleep(0.10) 
-            elif(motor == LEFT):
-                if(degree < self.left): # trying to retract the right arm ex: 180 -> 40
-                    for i in range(self.left, degree ,-STEP):
-                        command = f'{motor}{i}|'.encode()
-                        self.arduino_conn.write(command)
-                        time.sleep(0.10) 
-                else: # ex: 40 -> 180
-                    for i in range(self.left, degree ,STEP):
-                        command = f'{motor}{i}|'.encode()
-                        self.arduino_conn.write(command)
-                        time.sleep(0.10)
-            else:
-                command = f'{motor}{degree}|'.encode()
+            commands = motor.rotate(degree)
+            if(commands == None):
+                return
+            print(f'the motor is not null')
+            print(commands)
+            for command in commands:
                 self.arduino_conn.write(command)
-                time.sleep(0.10) 
+                time.sleep(0.1)
+        motor.update(degree)
 
+    def parallel_execute(self,first_motor,second_motor,first_deg,second_deg):
+        print(f'Both {first_motor} and {second_motor} are going to {first_deg} and {second_deg} at the same time')
+        f_commands = first_motor.rotate(first_deg) # ex: 5 commands
+        s_commands = second_motor.rotate(second_deg) # ex: 6 commands
+        commands = merge_alternatively(f_commands,s_commands)
+        for command in commands:
+            self.arduino_conn.write(command)
+            time.sleep(0.1)
+        first_motor.update(first_deg)
+        second_motor.update(second_deg)
 
     def arm_initialize(self,gripper_to_be_open):
-        self.execute(RIGHT,26)  # move right motor to 40 degrees.
-        self.right = 26
-        self.execute(LEFT,80)  # move left motor to 60 degrees.
-        self.left = 80
-        self.execute(BASE,150)  # move base motor to 90 degrees.
-        self.base = 150
+        self.execute(self.right,26)  # move right motor to 40 degrees.
+        self.execute(self.left,80)  # move left motor to 60 degrees.
+        self.execute(self.base,150)  # move base motor to 90 degrees.
         if(gripper_to_be_open):
             self.open_gripper()
 
+    #TODO : to be deleted 
     def horizontal_pickup(self):
         while(self.right > 25 or self.left < 85):
           if(self.left < 90): 
@@ -134,37 +124,40 @@ class Arm():
 
             # move to initial square
             print(f'Going to {initial_sq}')
-            self.execute(LEFT,moves[initial_sq][1])
-            self.left = moves[initial_sq][1]
+            self.parallel_execute(self.right,self.left,25,95)
+            time.sleep(0.5)
+            
+            self.execute(self.base,moves[initial_sq][2])   
             time.sleep(1)
-            self.execute(BASE,moves[initial_sq][2])
-            self.base = moves[initial_sq][2]
+
+            self.parallel_execute(self.left,self.right,moves[initial_sq][1],moves[initial_sq][0])
             time.sleep(1)
-            self.execute(RIGHT,moves[initial_sq][0])
-            self.right = moves[initial_sq][0]
-            time.sleep(2)
+            
+            
             self.grab()
             time.sleep(2)
 
             #! horizontal pick up
-            self.horizontal_pickup()
+            # self.horizontal_pickup()
+            print('preforming pick up motion')
+            self.parallel_execute(self.left,self.right,95,25)
 
             # move to initial pos
-            print('Back to initial pos')
-            self.execute(BASE,150)
+            # print('Back to initial pos')
+            # self.execute(BASE,150)
             # self.arm_initialize(gripper_to_be_open= False)
-            time.sleep(2)
+            time.sleep(1)
             
             # move to Target square
             print(f'Going to {target_sq}')
-            self.execute(LEFT,moves[target_sq][1])
-            self.left = moves[target_sq][1]
+            self.execute(self.base,moves[target_sq][2])
             time.sleep(1)
-            self.execute(BASE,moves[target_sq][2])
-            self.base = moves[target_sq][2]
-            time.sleep(1)
-            self.execute(RIGHT,moves[target_sq][0])
-            self.right = moves[target_sq][0]
+            self.parallel_execute(self.left,self.right,moves[target_sq][1],moves[target_sq][0])
+            # self.execute(LEFT,moves[target_sq][1])
+            # self.left = moves[target_sq][1]
+            # time.sleep(1)
+            # self.execute(RIGHT,moves[target_sq][0])
+            # self.right = moves[target_sq][0]
             time.sleep(1)
             self.open_gripper()
             time.sleep(1)
@@ -178,3 +171,9 @@ class Arm():
         except Exception as e:
             print('something went wrong !')
             print(f'Here is there Error : {e}')
+
+    def __str__(self):
+        # if (self.base.pos == None and self.right.pos == None and self.left.pos == None):
+        #     return 'motors not initialized yet !'
+        # else:
+        return f'base @ {self.base.pos}, right @ {self.right.pos}, left @ {self.left.pos}'
