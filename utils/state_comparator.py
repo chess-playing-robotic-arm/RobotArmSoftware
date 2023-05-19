0725c52b53147f6b143684c109eb2554cf27fb68
Youssef Darahem <youssef.darahem@gmail.com>
2023-05-19 15:29:46 +0300

    added state comparator

--- utils/state_comparator.py
+++ utils/state_comparator.py
@@ -1,4 +1,5 @@
 import cv2
+import multiprocessing
 
 # Load the two images you want to compare
 img1 = cv2.imread("images/test01.png")
@@ -30,3 +31,52 @@ if percentage_diff > 30:
     print("The images have changed significantly")
 else:
     print("The images are practically the same")
+
+def check_image_change(old_state, new_state, start_row, end_row, result_queue):
+    for i in range(start_row, end_row):
+        for j in range(8):
+            gray1 = cv2.cvtColor(old_state[i][j], cv2.COLOR_BGR2GRAY)
+            gray2 = cv2.cvtColor(new_state[i][j], cv2.COLOR_BGR2GRAY)
+            # Calculate the absolute difference between the two grayscale images
+            diff = cv2.absdiff(gray1, gray2)
+            # Apply a threshold to the absolute difference image to create a binary image
+            thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)[1]
+            percentage_diff = cv2.countNonZero(thresh) / thresh.size * 100
+            if percentage_diff > 30:
+                result_queue.put(new_state[i][j])
+                # print("The images have changed significantly")
+            else:
+                return
+            # print(percentage_diff)
+
+    # Compare the percentage of differing pixels to a threshold value
+    
+        # print("The images are practically the same")
+
+    
+
+
+def parallel_image_change_detection(old_board, new_board):
+    num_processes = multiprocessing.cpu_count()
+    result_queue = multiprocessing.Queue()
+
+    processes = []
+    rows_per_process = 8 // num_processes
+
+    for i in range(num_processes):
+        start_row = i * rows_per_process
+        end_row = (i + 1) * rows_per_process
+
+        p = multiprocessing.Process(target=check_image_change, args=(old_board, new_board,start_row,end_row, result_queue))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    differing_images = []
+    while not result_queue.empty():
+        images = result_queue.get()
+        differing_images.extend(images)
+
+    return differing_images
